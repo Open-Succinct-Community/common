22962323117b5cab26369eb574fb316feb9d6bd9
Venky <venkatramanm@gmail.com>
2021-09-28 23:33:19 +0530

    tests

---
+++ src/test/java/com/venky/core/math/DoubleUtilsTest.java
@@ -0,0 +1,13 @@
+package com.venky.core.math;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DoubleUtilsTest {
+    @Test
+    public void testRound(){
+        Assert.assertEquals(0,DoubleUtils.compareTo(1299.19, 1299.2));
+
+
+    }
+}
