161e75a8e532957f83191149d44141f339d2400f
Venky <venkatramanm@gmail.com>
2021-10-10 16:35:16 +0530

    tests

--- src/test/java/com/venky/core/date/DateUtilTest.java
+++ src/test/java/com/venky/core/date/DateUtilTest.java
@@ -1,7 +1,11 @@
 package com.venky.core.date;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -11,5 +15,13 @@ public class DateUtilTest {
 	public void test() throws ParseException {
 		new SimpleDateFormat(DateUtils.ISO_DATE_FORMAT_STR).parse("2016-06-24");
 	}
+	@Test
+	public void testIsoDateTime() {
+		DateFormat format = DateUtils.getFormat("yyyy-MM-dd'T'HH:mm:ss");
+		format.setTimeZone(TimeZone.getTimeZone("UTC"));
+		String s = format.format(new Date());
+		System.out.println(s);
+		LocalDateTime time = LocalDateTime.parse("2021-06-15T13:40:25.040");
 
+	}
 }
