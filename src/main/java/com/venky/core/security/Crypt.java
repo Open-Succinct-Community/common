e05818e04066274b93bf0647251c2f479ce9b43a
Venky <venkatramanm@gmail.com>
2025-02-11 19:49:00 +0530

    BC library for Removed

--- src/main/java/com/venky/core/security/Crypt.java
+++ src/main/java/com/venky/core/security/Crypt.java
@@ -1,11 +1,12 @@
 package com.venky.core.security;
 
 import com.venky.cache.Cache;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.KeyFactory;
@@ -13,50 +14,41 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.Security;
 import java.security.Signature;
+import java.security.spec.DSAPublicKeySpec;
+import java.security.spec.ECPublicKeySpec;
+import java.security.spec.EdECPoint;
+import java.security.spec.EdECPrivateKeySpec;
+import java.security.spec.EdECPublicKeySpec;
+import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.NamedParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.security.spec.XECPrivateKeySpec;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class Crypt {
-    static {
-        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null){
-            Security.insertProviderAt(new BouncyCastleProvider(),1);
-        }
+    private Crypt(){
     }
-
-    String provider;
-    private Crypt(String provider){
-        this.provider = provider;
-    }
-    static ThreadLocal<Cache<String,Crypt>> cachedInstancesByProvider = new ThreadLocal<>();
-    public static Crypt getInstance(String provider) {
-        Cache<String,Crypt> cache = cachedInstancesByProvider.get();
-        if (cache == null){
-            cache = new Cache<String, Crypt>() {
-                @Override
-                protected Crypt getValue(String provider) {
-                    return new Crypt(provider);
-                }
-            };
-            cachedInstancesByProvider.set(cache);
-        }
-        return cache.get(provider);
-    }
+    static ThreadLocal<Crypt> cryptInstance = new ThreadLocal<>();
     public static Crypt getInstance() {
-        return getInstance(BouncyCastleProvider.PROVIDER_NAME);
+        Crypt crypt =  cryptInstance.get();
+        if (crypt == null){
+            crypt = new Crypt();
+            cryptInstance.set(crypt);
+        }
+        return cryptInstance.get();
     }
-
-
+   
     public String getBase64Encoded(Key key){
         byte[] encoded = key.getEncoded();
-        String b64Key = Base64.getEncoder().encodeToString(encoded);
-        return b64Key;
+        return Base64.getEncoder().encodeToString(encoded);
     }
     public SecretKey getSecretKey(String algo,String base64Key){
         return new SecretKeySpec(Base64.getDecoder().decode(base64Key),algo);
@@ -64,35 +56,114 @@ public class Crypt {
 
     public PublicKey getPublicKey(String algo,String base64PublicKey){
         byte [] binCpk = Base64.getDecoder().decode(base64PublicKey);
-        X509EncodedKeySpec pkSpec = new X509EncodedKeySpec(binCpk);
+        return getPublicKey(algo,binCpk);
+    }
 
-        try {
-            KeyFactory keyFactory = KeyFactory.getInstance(algo,provider);
-            PublicKey pKey = keyFactory.generatePublic(pkSpec);
-            return pKey;
-        }catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeySpecException ex){
-            throw new RuntimeException(ex);
+    public PublicKey getPublicKey(String algo,byte[] binCpk){
+        PublicKey key = null;
+        
+        for (KeySpec spec : getPublicKeySpecs(algo,binCpk)){
+            key = getPublicKey(algo,spec);
+            if (key != null){
+                break;
+            }
+        }
+        return key;
+    }
+    private List<KeySpec> getPublicKeySpecs(String algo, byte[] binCpk){
+        List<KeySpec> specs = new ArrayList<>();
+        specs.add(new X509EncodedKeySpec(binCpk));
+        specs.add(new EdECPublicKeySpec(NamedParameterSpec.ED25519,byteArrayToEdPoint(binCpk)));
+        
+        return specs;
+    }
+    private static EdECPoint byteArrayToEdPoint(byte[] arr){
+        byte msb = arr[arr.length - 1];
+        boolean xOdd = (msb & 0x80) != 0;
+        arr[arr.length - 1] &= (byte) 0x7F;
+        reverse(arr);
+        BigInteger y = new BigInteger(1, arr);
+        return new EdECPoint(xOdd, y);
+    }
+    private static void reverse(byte [] arr){
+        int i = 0;
+        int j = arr.length - 1;
+        
+        while(i < j)
+        {
+            swap(arr, i, j);
+            i++;
+            j--;
         }
     }
+    
+    private static void swap(byte[] arr, int i, int j){
+        byte tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
+    public PublicKey getPublicKey(String algo,  KeySpec spec){
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance(algo);
+            return keyFactory.generatePublic(spec);
+        }catch (NoSuchAlgorithmException | InvalidKeySpecException ex){
+            return null;
+        }
+    }
+    private List<KeySpec> getPrivateKeySpecs(String algo, byte[] binCpk){
+        List<KeySpec> specs = new ArrayList<>();
+        specs.add(new PKCS8EncodedKeySpec(binCpk));
+        if (algo.startsWith("Ed")) {
+            specs.add(new EdECPrivateKeySpec(new NamedParameterSpec(algo), binCpk));
+        }
+        if (algo.startsWith("X")){
+            specs.add(new XECPrivateKeySpec(new NamedParameterSpec(algo), binCpk));
+        }
+        try {
+            specs.add(new DESKeySpec(binCpk));
+        }catch (Exception ex){
+            //
+        }
+        specs.add(new EncodedKeySpec(binCpk) {
+            @Override
+            public String getFormat() {
+                return "RAW";
+            }
+        });
+        return specs;
+    }
+    
     public PrivateKey getPrivateKey(String algo,String base64PrivateKey){
         byte [] binCpk = Base64.getDecoder().decode(base64PrivateKey);
-        PKCS8EncodedKeySpec pkSpec = new PKCS8EncodedKeySpec(binCpk);
-
+        return getPrivateKey(algo,binCpk);
+    }
+    public PrivateKey getPrivateKey(String algo,byte[] binCpk){
+        PrivateKey key = null;
+        for (KeySpec spec : getPrivateKeySpecs(algo,binCpk)){
+            key = getPrivateKey(algo,spec);
+            if (key != null){
+                break;
+            }
+        }
+        return key;
+    }
+    
+    public PrivateKey getPrivateKey(String algo, KeySpec spec){
         try {
-            KeyFactory keyFactory = KeyFactory.getInstance(algo,provider);
-            PrivateKey pKey = keyFactory.generatePrivate(pkSpec);
-            return pKey;
-        }catch (NoSuchAlgorithmException | NoSuchProviderException |InvalidKeySpecException ex){
-            throw new RuntimeException(ex);
+            KeyFactory keyFactory = KeyFactory.getInstance(algo);
+            return keyFactory.generatePrivate(spec);
+        }catch (NoSuchAlgorithmException | InvalidKeySpecException ex){
+            return null;
         }
     }
 
     public KeyPair generateKeyPair(String algo,int strength ) {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algo,provider);
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(algo);
             keyPairGenerator.initialize(strength);
             return keyPairGenerator.generateKeyPair();
-        }catch (NoSuchAlgorithmException | NoSuchProviderException ex){
+        }catch (NoSuchAlgorithmException ex){
             throw new RuntimeException(ex);
         }
     }
@@ -107,7 +178,7 @@ public class Crypt {
     
     public String generateSignature(byte[] payload, String signatureAlgorithm , PrivateKey privateKey)  {
         try {
-            Signature signature = Signature.getInstance(signatureAlgorithm, provider); //
+            Signature signature = Signature.getInstance(signatureAlgorithm); //
             signature.initSign(privateKey);
             signature.update(payload);
             return Base64.getEncoder().encodeToString(signature.sign());
@@ -122,7 +193,7 @@ public class Crypt {
     public boolean verifySignature(byte[] data, String signature, String signatureAlgorithm ,PublicKey pKey){
         byte [] signatureBytes = Base64.getDecoder().decode(signature);
         try {
-            Signature s = Signature.getInstance(signatureAlgorithm,provider);
+            Signature s = Signature.getInstance(signatureAlgorithm);
             s.initVerify(pKey);
             s.update(data);
             return s.verify(signatureBytes);
@@ -136,7 +207,7 @@ public class Crypt {
         @Override
         protected Cipher getValue(String algorithm) {
             try {
-                return Cipher.getInstance(algorithm,provider);
+                return Cipher.getInstance(algorithm);
             }catch (Exception ex) {
                 throw new RuntimeException(ex);
             }
@@ -191,7 +262,7 @@ public class Crypt {
     }
     public byte[] digest(String algorithm,byte[] data){
         try {
-            MessageDigest digest = MessageDigest.getInstance(algorithm, provider);
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
             digest.reset();
             digest.update(data);
             return digest.digest();
